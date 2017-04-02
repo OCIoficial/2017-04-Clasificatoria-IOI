f19c6a47e39664102b74d2867d91f39dd9086bf0
Nico Lehmann <nlehmann@dcc.uchile.cl>
2017-04-02 18:11:21 -0300

    Partial solutions for teletransportador

---
+++ teletransportador/solutions/partial/nlehmann_st2.cpp
@@ -0,0 +1,35 @@
+/*
+ * -----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <nlehmann@dcc.uchile.cl> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return Nicolás Lehmann
+ * -----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N), B(N);
+  for (int i = 0; i < N; ++i)
+    cin >> A[i];
+  for (int i = 0; i < N; ++i)
+    cin >> B[i];
+
+  int min = INT_MAX;
+  for (int C = 1; C <= 10; ++C) {
+    int cost = 0;
+    for (int i = 0; i < N; ++i)
+      cost += abs(A[i]-C)*B[i];
+    if (cost < min)
+      min = cost;
+  }
+  cout << min << "\n";
+
+  return 0;
+}
