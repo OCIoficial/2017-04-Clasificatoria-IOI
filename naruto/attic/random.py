0bab8507f5fd1a15ed2033b8c22b18f64a3f9512
Martin Munoz <mmunos@uc.cl>
2017-04-03 16:52:20 -0300

    naruto ver. 1

---
+++ naruto/attic/random.py
@@ -0,0 +1,28 @@
+import random
+import sys
+
+k = int(sys.argv[1])
+n = int(sys.argv[2])
+seed = int(sys.argv[3])
+
+random.seed(hash(seed))
+
+if(k == 1):
+	a = 0
+	b = 0
+	c = random.randint(0,n)
+if(k == 2):
+	a = 0
+	b = random.randint(0,n)
+	c = random.randint(b,b+2)
+if(k == 3):
+	a = random.randint(0,n)
+	b = random.randint(a,a+2)
+	c = random.randint(b,a+2)
+if(k == 4):
+	a = random.randint(0,n)
+	b = random.randint(a,n)
+	c = random.randint(b,n)
+
+print(a,b,c)
+
